f85547084cbc195db6744326ed3cf7194cbfe3ca
danielpaul <danielpaul2008@gmail.com>
2015-05-18 18:57:20 +0100

    added bit subtracting by hand notes

--- Bit Manipulation/bits.java
+++ Bit Manipulation/bits.java
@@ -84,7 +84,7 @@ public class bits {
           01000 >> 00010 = 0010
 
           -8 >> 2 = -2
-          11..111000 >> 00...0010 = 1111...11110
+          11...111000 >> 00...0010 = 1111...11110
     */
     System.out.println(">> signed right shift: 8 >> 2: " + (8 >> 2));
     System.out.println(">> signed right shift: -8 >> 2: " + (-8 >> 2));
@@ -95,10 +95,10 @@ public class bits {
           Shifts the bits of the first operand right by the number of bits specified by the second operand; 0s are filled in from the left.
           
           8 >>> 2 = 2
-          00..01000 >> 000..0010 = 000..00010
+          00...01000 >> 000...0010 = 000...00010
 
           -8 >>> 2 = -2
-          11..111000 >> 00...0010 = 00111...11110
+          11...111000 >> 00...0010 = 00111...11110
 
     */
     System.out.println(">>> unsigned right shift: 8 >>> 2: " + (8 >>> 2));
@@ -111,10 +111,46 @@ public class bits {
           All 0 bits are set to 1, and all 1 bits are set to 0.
 
           ~5 = -6
-          ~00..00101 = 11..11010
+          ~00...00101 = 11...11010
     */
     System.out.println("~ bitwise complement: ~5: " + (~5));
+
+
   }
+
+
+
+  /* ---------- Doing subtraction by hand ----------
+
+    1 1 0 1 0 1 1 0 0 0
+  -                   1
+  ----------------------
+    1 1 0 1 0 1 0 1 1 1
+
+  * When you substract 1 from a number you look at the least significant bit.
+  * If it's a 1 you change it to a 0 and you're done.
+  * If it's a zero, you must borrow from a larget bit, so you go to increasingly larger bits, changing each from a zero to a 1, ultil you find a 1.
+  * You flip that to a 0 and you're done.
+  
+
+  (n & (n-1) == 0)
+    What does it mean if A & B == 0?
+      
+      * It means that A and B never have a bit in the same place.
+      * So if n & (n-1) == 0, then n and n-1 never share a 1.
+
+      * Therefore all the Xs below must be zeros.
+
+      X X X X X X 1 0 0 0
+    -                   1
+    ----------------------
+      X X X X X X 0 1 1 1
+
+      * So the number n must look like this: 00001000.
+      * n is therefore a power of two.
+      * ((n & (n-1)) == 0) checks if n is a power of 2.
+
+  */
 
 
 
