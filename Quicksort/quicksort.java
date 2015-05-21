e0e238bbb89abf6f891ed6c0b480f1aab6553de1
danielpaul <danielpaul2008@gmail.com>
2015-05-21 19:21:57 +0100

    Working on quick sort

---
+++ Quicksort/quicksort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class quicksort {
+
+  public static void main(String args[]) {
+
+    int[] numbers = {6,5,1,3,8,4,7,9,2};
+
+    System.out.println("Unsorted Numbers: " + Arrays.toString(numbers));
+
+    quicksort(numbers);
+
+    System.out.println("Sorted Numbers: " + Arrays.toString(numbers));
+
+  }
+
+
+  public static void quicksort(int[] numbers) {
+
+    // ...
+
+  }
+
+
+}
